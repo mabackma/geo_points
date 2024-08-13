03630a26d089b773c46809bb68362274dc6595ef
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-08-13 10:15:44 +0300

    draws points inside polygon

--- src/main.rs
+++ src/main.rs
@@ -65,8 +65,8 @@ fn generate_random_points(p: &Polygon, amount: i32) -> Vec<Coord<f64>> {
     points
 }
 
-// Map polygon coordinates to image pixel coordinates
-fn map_coordinates_to_image(p: &Polygon<f64>, img_width: u32, img_height: u32) -> Vec<(u32, u32)> {
+// Scale polygon to fit image
+fn scale_x_and_y(p: &Polygon<f64>, img_width: u32, img_height: u32) -> (f64, f64, f64, f64) {
     let (min_x, max_x, min_y, max_y) = get_min_max_coordinates(&p);
     let width = max_x - min_x;
     let height = max_y - min_y;
@@ -74,6 +74,13 @@ fn map_coordinates_to_image(p: &Polygon<f64>, img_width: u32, img_height: u32) -
     // Scale polygon to fit image
     let scale_x = img_width as f64 / width;
     let scale_y = img_height as f64 / height;
+
+    (scale_x, scale_y, min_x, min_y)
+}
+
+// Map polygon coordinates to image pixel coordinates
+fn map_coordinates_to_image(p: &Polygon<f64>, img_width: u32, img_height: u32) -> Vec<(u32, u32)> {
+    let (scale_x, scale_y, min_x, min_y) = scale_x_and_y(p, img_width, img_height);
 
     p.exterior()
         .points()
@@ -112,8 +119,16 @@ fn draw_line_segment(img: &mut RgbImage, p1: (u32, u32), p2: (u32, u32), color: 
     }
 }
 
-// Draw polygon
-fn draw_polygon(p: Polygon) {
+// Draws a random point
+fn draw_random_point(img: &mut RgbImage, p: &Polygon, img_width: u32, img_height: u32, point: Coord, color: Rgb<u8>) {
+    let (scale_x, scale_y, min_x, min_y) = scale_x_and_y(&p, img_width, img_height);
+    let x = ((point.x - min_x) * scale_x).round() as u32;
+    let y = (img_height as f64 - (point.y - min_y) * scale_y).round() as u32;
+    img.put_pixel(x, y, color);
+} 
+
+// Draw image with polygon and random points
+fn draw_image(p: &Polygon, rand_p: Vec<Coord<f64>>) {
     let img_width = 800;
     let img_height = 600;
     let mut img = RgbImage::new(img_width, img_height);
@@ -126,6 +141,11 @@ fn draw_polygon(p: Polygon) {
         let (x0, y0) = mapped_coordinates[i];
         let (x1, y1) = mapped_coordinates[(i + 1) % mapped_coordinates.len()]; // Wrap around to connect the last point to the first
         draw_line_segment(&mut img, (x0, y0), (x1, y1), Rgb([0, 0, 255]));
+    }
+
+    // Draw the generated random points within the polygon
+    for point in rand_p {
+        draw_random_point(&mut img, &p, img_width, img_height, point, Rgb([255, 0, 0])) // Draw points in red
     }
 
     img.save("polygon_image.png").expect("Failed to save image");
@@ -154,7 +174,6 @@ fn main() {
     let random_points = generate_random_points(&polygon, 10);
     println!("random_points within polygon: {:?}", random_points);  
 
-    // Draw polygon
-    draw_polygon(polygon);
+    draw_image(&polygon, random_points);
 }
 
