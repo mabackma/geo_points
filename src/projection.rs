71e440010278b3da099a34e69d86daaee45213ba
Matias Hiltunen <26164588+MatiasHiltunen@users.noreply.github.com>
2024-08-28 10:39:46 +0300

    add comments

--- src/projection.rs
+++ src/projection.rs
@@ -11,8 +11,8 @@ fn test_projection() {
     // EPSG:4326 - WGS84
     let to = Proj::from_proj_string("+proj=longlat +datum=WGS84 +no_defs +type=crs")
     .unwrap();
-/*  N=7369787.000, E=427754.979
-    N=7369564.333, E=427997.035 */
+    
+    /*  N=7369564.333, E=427997.035 */
     let epsg3067_northern = 7369564.333;
     let epsg3067_eastern = 427997.035;
 
@@ -31,6 +31,6 @@ fn test_projection() {
     assert!(((latitude.to_degrees() * 1E6).round() / 1E6) == 66.437124, "projection match with lat");
     assert!(((longitude.to_degrees() * 1E6).round() / 1E6) == 25.385742, "projection match with lon");
 
-    // longitude 25°23'8.67" = 25.385742
-    // latitude 66°26'13.646" = 66.437124
+    // EPSG:3067 E 427997.035 -> EPSG:4326 longitude 25°23'8.67" = 25.385742
+    // EPSG:3067 N 7369564.333 -> EPSG:4326 latitude 66°26'13.646" = 66.437124
 }
